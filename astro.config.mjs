c0659d269b7b5be9be2d6980d584c64be4286e4f
Palak Porwal <76276573+SpaceExp263@users.noreply.github.com>
2024-08-13 15:53:18 +0000

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://palakporwal.portfolio',
-  base:'developerportfolio',
+  site: 'https://palakpwl07.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
